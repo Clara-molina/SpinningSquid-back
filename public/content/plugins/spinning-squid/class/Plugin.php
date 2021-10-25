d7dc5bc2a7e043918ac953a12373cbaaa266cd58
RomainTalbot <romain.talbot.33@gmail.com>
2021-10-25 13:56:26 +0200

    methods for create CPT : Skatepark + Sale

--- public/content/plugins/spinning-squid/class/Plugin.php
+++ public/content/plugins/spinning-squid/class/Plugin.php
@@ -7,6 +7,8 @@ class Plugin
     public function __construct()
     {
         add_action('init',[$this,'createArticlePostType']);
+        add_action('init',[$this,'createSkateparkPostType']);
+        add_action('init',[$this,'createSalePostType']);
     }
 
     //Méthode créant un CPT : Post (forum)
@@ -18,7 +20,7 @@ class Plugin
                 'public' => true,
                 'hierarchical' => false,
                 'menu_icon' => 'dashicons-welcome-write-blog',
-                'supports' => [ // je définis les fonctionnalités dont va bénéficier le CPT
+                'supports' => [ 
                     'title',
                     'thumbnail',
                     'editor',
@@ -34,7 +36,51 @@ class Plugin
     }
 
     //Méthode créant un CPT : SkatePark 
+    public function createSkateparkPostType()
+    {
+        register_post_type('skatepark',
+            [
+                'label' => 'Skatepark',
+                'public' => true,
+                'hierarchical' => false,
+                'menu_icon' => 'dashicons-location-alt',
+                'supports' => [ 
+                    'title',
+                    'thumbnail',
+                    'editor',
+                    'author',
+                    'excerpt',
+                    'comments'    
+                ],
+                'capability_type' => 'post',
+                'map_meta_cap' => true,
+                'show_in_rest' => true
+            ]
+        );
+    }
 
     //Méthode créant un CPT : Sale 
+    public function createSalePostType()
+    {
+        register_post_type('sale',
+            [
+                'label' => 'Sale',
+                'public' => true,
+                'hierarchical' => false,
+                'menu_icon' => 'dashicons-cart',
+                'supports' => [ 
+                    'title',
+                    'thumbnail',
+                    'editor',
+                    'author',
+                    'excerpt',
+                    'comments'    
+                ],
+                'capability_type' => 'post',
+                'map_meta_cap' => true,
+                'show_in_rest' => true
+            ]
+        );
+    }
     
 }
