c381cf36aba6bf4849f985177486d983ae490d68
RomainTalbot <romain.talbot.33@gmail.com>
2021-11-04 11:30:01 +0100

    add a plugin + add method to add user's profil image

--- public/content/plugins/spinning-squid/class/API.php
+++ public/content/plugins/spinning-squid/class/API.php
@@ -50,7 +50,6 @@ class API
 
         //$userID = wp_create_user($username, $password, $email);
 
-
         $userData = [
             'user_login' => $username,
             'first_name' => $firstname,
@@ -113,6 +112,44 @@ class API
         );
 
         update_user_meta($userID, $userData, false);
+
+        // update user's avatar :
+        $image = $request->get_param('image');
+
+        // Je récupère la base64 et le type de l'image
+        list($type, $data) = explode(';', $image);
+        list(, $data)      = explode(',', $data);
+        list(, $type) = explode('/', $type);
+
+    
+        // Si l'image a le bont type alors...
+        if (!in_array($type, ['jpg', 'jpeg','png'])) {
+            echo "nop!";
+        } else {
+            echo "yes!";
+            $dataDecoded = base64_decode($data);
+            //$datajson = $dataDecoded;
+        }
+    
+        // nom de mon image
+        $name = $username . '-' . uniqid() . $type;
+        // nom de mon image (sans l'extension)
+        $filename = basename( $name );
+        // je demande à WP les chemins de téléchargement 
+        $upload_dir = wp_upload_dir();
+
+        // si il n'existe pas, WP va me créer un dossier (ici uploads/2021/)
+        if ( wp_mkdir_p( $upload_dir['path'] ) ) {
+            $file = $upload_dir['path'] . '/' . $filename;
+        }
+        else {
+            $file = $upload_dir['basedir'] . '/' . $filename;
+        }
+        
+        // Je reconstruit mon image
+        file_put_contents( $file, $dataDecoded );
+
+        update_user_meta($userID, 'profilImage', $file);
     }
 
     // Supprimer un utilisateur 
@@ -133,6 +170,8 @@ class API
         $streetspot = $request->get_param('streetspot');
         $zipcode = $request->get_param('zipcode');
         $city = $request->get_param('city');
+        $latitude = $request->get_param('latitude');
+        $longitude = $request->get_param('longitude');
         $parking = $request->get_param('parking');
         $water = $request->get_param('water');
         $trashcan = $request->get_param('trashcan');
@@ -166,6 +205,8 @@ class API
                 update_post_meta($skateparkCreateResult, 'street', $street);
                 update_post_meta($skateparkCreateResult, 'zipcode', $zipcode);
                 update_post_meta($skateparkCreateResult, 'city', $city);
+                update_post_meta($skateparkCreateResult, 'city', $latitude);
+                update_post_meta($skateparkCreateResult, 'city', $longitude);
                 update_post_meta($skateparkCreateResult, 'parking', $parking);
                 update_post_meta($skateparkCreateResult, 'water', $water);
                 update_post_meta($skateparkCreateResult, 'trashcan', $trashcan);
@@ -178,8 +219,7 @@ class API
                 list($type, $data) = explode(';', $image);
                 list(, $data)      = explode(',', $data);
                 list(, $type) = explode('/', $type);
-
-            
+           
                 // Si l'image a le bont type alors...
                 if (!in_array($type, ['jpg', 'jpeg','png'])) {
                     echo "nop!";
@@ -191,7 +231,8 @@ class API
                 }
             
                 // nom de mon image
-                $name = $title . '-' . uniqid() . $type;
+                $userImageID = uniqid();
+                $name = $userImageID.$type;
                 // nom de mon image (sans l'extension)
                 $filename = basename( $name );
                 // je demande à WP les chemins de téléchargement 
@@ -211,7 +252,7 @@ class API
                 $attachment = array(
                 //'guid'=> $upload_dir['url'] . '/' . basename($name),
                 'post_mime_type' => "image/{$type}",
-                'post_title' => 'test',
+                'post_title' => $title,
                 'post_content' => '',
                 'post_status' => 'inherit'
                 );
@@ -224,8 +265,14 @@ class API
                 $attach_data = wp_generate_attachment_metadata($image_id, $file);
                 wp_update_attachment_metadata($image_id, $attach_data);
 
+                // Ajout de l'image d'en-tête
+                set_post_thumbnail($skateparkCreateResult, $image_id);
+
                 return [
                     'succes' => true,
+                    'title' => $title,
+                    'parking' => $parking,
+                    'image' => $image
                     //'data' => $datajson
                  ];
             }
@@ -246,6 +293,8 @@ class API
         $streetspot = $request->get_param('streetspot');
         $zipcode = $request->get_param('zipcode');
         $city = $request->get_param('city');
+        $latitude = $request->get_param('latitude');
+        $longitude = $request->get_param('longitude');
         $parking = $request->get_param('parking');
         $water = $request->get_param('water');
         $trashcan = $request->get_param('trashcan');
@@ -257,7 +306,7 @@ class API
         //image est envoyé par le front en base64
         $image = $request->get_param('image');
 
-        $user = wp_get_current_user();
+        //$user = wp_get_current_user();
 
         // Je vérie que l'user a le bon rôle (donc bien inscrit)
       
@@ -279,6 +328,8 @@ class API
                 update_post_meta($skateparkCreateResult, 'street', $street);
                 update_post_meta($skateparkCreateResult, 'zipcode', $zipcode);
                 update_post_meta($skateparkCreateResult, 'city', $city);
+                update_post_meta($skateparkCreateResult, 'city', $latitude);
+                update_post_meta($skateparkCreateResult, 'city', $longitude);
                 update_post_meta($skateparkCreateResult, 'parking', $parking);
                 update_post_meta($skateparkCreateResult, 'water', $water);
                 update_post_meta($skateparkCreateResult, 'trashcan', $trashcan);
@@ -324,7 +375,7 @@ class API
                 $attachment = array(
                 //'guid'=> $upload_dir['url'] . '/' . basename($name),
                 'post_mime_type' => "image/{$type}",
-                'post_title' => 'test',
+                'post_title' => $title,
                 'post_content' => '',
                 'post_status' => 'inherit'
                 );
@@ -336,6 +387,9 @@ class API
                 // Generate the metadata for the attachment, and update the database record.
                 $attach_data = wp_generate_attachment_metadata($image_id, $file);
                 wp_update_attachment_metadata($image_id, $attach_data);
+
+                // Ajout de l'image d'en-tête
+                set_post_thumbnail($skateparkCreateResult, $image_id);
 
                 return [
                     'succes' => true,
